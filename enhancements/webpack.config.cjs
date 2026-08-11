6d0729a2fd78ca53392b01aba41e5aa71ca3adee
meysam82003 <meysamsalamatnasab@gmail.com>
2026-08-11 21:56:11 +0330

    fix: configure Webpack Node fallbacks for browser MTProto

--- enhancements/webpack.config.cjs
+++ enhancements/webpack.config.cjs
@@ -5,13 +5,26 @@ module.exports = {
   entry: './telegram-web-entry.js',
   output: { path: path.resolve(__dirname, 'dist'), filename: 'telegram-web.bundle.js' },
   resolve: {
+    preferRelative: true,
     fallback: {
       buffer: require.resolve('buffer/'),
       process: require.resolve('process/browser'),
       events: require.resolve('events/'),
       stream: require.resolve('stream-browserify'),
       crypto: require.resolve('crypto-browserify'),
-      fs: false, net: false, tls: false, child_process: false
+      util: require.resolve('util/'),
+      assert: require.resolve('assert/'),
+      constants: require.resolve('constants-browserify'),
+      path: require.resolve('path-browserify'),
+      os: require.resolve('os-browserify/browser'),
+      vm: require.resolve('vm-browserify'),
+      url: require.resolve('url/'),
+      querystring: require.resolve('querystring-es3'),
+      http: require.resolve('stream-http'),
+      https: require.resolve('https-browserify'),
+      zlib: require.resolve('browserify-zlib'),
+      tty: require.resolve('tty-browserify'),
+      fs: false, net: false, tls: false, child_process: false, worker_threads: false
     }
   },
   plugins: [
